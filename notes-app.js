c4dbf4a4df9c46406cca73e4e855dbe3469da787
Maria Garcia <mariagarciamontes06@outlook.com>
2023-12-27 20:28:33 +0100

    Handle errors, date and optimize code

--- notes-app.js
+++ notes-app.js
@@ -1,26 +1,41 @@
+'use strict';
+
 const notes = getSavedNotes();
 
 const filters = {
   searchText: "",
+  sortBy: "byEdited"
 };
 
 renderNotes(notes, filters);
 
-document.querySelector("#create-note").addEventListener("click", function (e) {
+document.querySelector("#create-note").addEventListener("click",  (e) => {
+  const timestamp = moment().valueOf();
   notes.push({
     id:uuidv4(),
     title: "",
     body: "",
+    createAt: timestamp,
+    updateAt: timestamp
   });
   saveNotes(notes)
-  renderNotes(notes, filters);
+  location.assign(`/edit.html#${notes[notes.length-1].id}`)
 });
 
-document.querySelector("#search-text").addEventListener("input", function (e) {
+document.querySelector("#search-text").addEventListener("input", (e) => {
   filters.searchText = e.target.value;
   renderNotes(notes, filters);
 });
 
-document.querySelector("#filter-by").addEventListener("change", function (e) {
-  console.log(e.target.value);
+document.querySelector("#filter-by").addEventListener("change", (e) => {
+  filters.sortBy = e.target.value;
+  renderNotes(notes, filters);
 });
+
+window.addEventListener('storage', (e) =>{
+  if(e.key === 'notes'){
+    notes === JSON.parse(e.newValue)
+    renderNotes(notes, filters)
+  } 
+})
+
